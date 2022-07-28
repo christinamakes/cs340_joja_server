b879c42ee5ee98ed7eea14ffb74219e00d97cb7f
Christina Moore <sayhitochristina@gmail.com>
2022-07-28 14:35:30 -0600

    add member post

--- app.js
+++ app.js
@@ -12,9 +12,13 @@ var app     = express();
 PORT        = process.env.PORT || 9124;
 
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 /*
     ROUTES
 */
+// MEMBERS
+// SELECT ALL
 app.get('/members', function(req, res)
     {  
         db.connection.query('SELECT * FROM Members;', function(error, results, fields){
@@ -22,6 +26,32 @@ app.get('/members', function(req, res)
         })
     });
 
+// ADD MEMBER
+app.post('/add-member',function(req,res)
+{
+    let data = req.body;
+    query1 = `INSERT INTO Members(name,email,address,phone) VALUES ('${data.name}','${data.email}','${data.address}','${data.phone}')`;
+    db.connection.query(query1, function(err,rows,fields){
+        if(err){
+            console.log(err)
+            res.sendStatus(400);
+        }
+        else
+        {
+            query2 = `SELECT * FROM Members;`;
+            db.connection.query(query2, function(err,rows,fields) {
+                if(err) {
+                    console.log(err);
+                    res.sendStatus(400);
+                }
+                else
+                {
+                    res.send(JSON.stringify(rows));
+                }
+            })
+        }
+    })
+})
 /*
     LISTENER
 */
