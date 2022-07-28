374aad0dfbe79570b28d6c69e8a908b074acc587
Christina Moore <sayhitochristina@gmail.com>
2022-07-28 13:05:47 -0600

    pool

--- database/db-connector.js
+++ database/db-connector.js
@@ -9,10 +9,6 @@ const connection = mysql.createPool({
     password: 'f11ae7e8',
     database: 'heroku_f2cbdce723a87d9'
   });
-  // open the MySQL connection
-  connection.connect(error => {
-    if (error) throw error;
-    console.log("Successfully connected to the database.");
-  });
+
 // Export it for use in our applicaiton
 module.exports.connection = connection;
