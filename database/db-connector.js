bdd6b3e2c38b18572f56ae6f0f2ea49e7d1ffbad
Christina Moore <sayhitochristina@gmail.com>
2022-07-28 15:26:19 -0600

    env

--- database/db-connector.js
+++ database/db-connector.js
@@ -2,13 +2,12 @@
 
 // Get an instance of mysql we can use in the app
 var mysql = require('mysql')
-const dbConfig = require("../.env");
 
 const connection = mysql.createPool({
-    host: dbConfig.DBHOST,
-    user: dbConfig.DBUSER,
-    password: dbConfig.DBPASSWORD,
-    database: dbConfig.DBDATABASE
+    host: process.env.DBHOST,
+    user: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
+    database: process.env.DBDATABASE
   });
 
 // Export it for use in our applicaiton
