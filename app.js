599fd02dfe43122339c24b2755897df2c2138bc1
Christina Moore <sayhitochristina@gmail.com>
2022-07-28 11:20:13 -0600

    port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var db = require('./database/db-connector')
 */
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 9124;                 // Set a port number at the top so it's easy to change in the future
+PORT        = process.env.PORT || 9124;                 // Set a port number at the top so it's easy to change in the future
 
 /*
     ROUTES
