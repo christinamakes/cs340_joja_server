b5453999d566ec4f5f040693bc8b1c3d2a439016
Devon Braner <68525598+orngepeel@users.noreply.github.com>
2022-08-04 14:31:43 -0400

    products router

---
+++ routes/product-router.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../database/db-connector')
+
+router.get('/', function(req, res)
+    {  
+        db.connection.query('SELECT * FROM Products;', function(error, results, fields){
+            res.send(JSON.stringify(results));
+        })
+    });
+
+// ADD PRODUCT
+router.post('/add',function(req,res)
+{
+    let data = req.body;
+    query1 = `INSERT INTO Products(product_name, product_price, season_code, number_in_stock) VALUES ('${data.product_name}','${data.product_price}','${data.season_code}','${data.number_in_stock}')`;
+    db.connection.query(query1, function(err,rows,fields){
+        if(err){
+            console.log(err)
+            res.sendStatus(400);
+        }
+        else
+        {
+            query2 = `SELECT * FROM Products;`;
+            db.connection.query(query2, function(err,rows,fields) {
+                if(err) {
+                    console.log(err);
+                    res.sendStatus(400);
+                }
+                else
+                {
+                    res.send(JSON.stringify(rows));
+                }
+            })
+        }
+    })
+});
+
+// UPDATE PRODUCT
+router.put('/update', function(req,res,next){
+    const data = req.body;
+    const product_id = parseInt(req.body.product_id);
+    const name = req.body.product_name;
+    const price = req.body.product_price;
+    const season_code = req.body.season_code;
+    const number_in_stock = req.body.number_in_stock;
+    const queryUpdateEmployee = `UPDATE Products SET ? WHERE product_id = ?`;
+
+          // Run the 1st query
+          db.connection.query(queryUpdateEmployee, [{product_name:name,product_price:price,season_code:season_code,number_in_stock:number_in_stock},product_id], function(error, rows, fields){
+              if (error) {
+              // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+              console.log(error);
+              res.sendStatus(400);
+              } else {
+                  res.sendStatus(200);
+              }
+  })});
+
+  module.exports = router
