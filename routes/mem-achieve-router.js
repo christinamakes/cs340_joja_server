15668288b5eae313038a14989b817c30d39acc06
Christina Moore <sayhitochristina@gmail.com>
2022-08-07 15:58:07 -0600

    again join

--- routes/mem-achieve-router.js
+++ routes/mem-achieve-router.js
@@ -3,34 +3,33 @@ const router = express.Router()
 const db = require('../database/db-connector')
 
 // GET MEMBER_ACHIEVEMENT
-router.get('/', function(req, res)
-    {  
-        const query = `SELECT Members.member_name, Achievements.achievement_title FROM Members, Achievements INNER JOIN Mem_Achieve_Details ON Mem_Achieve_Details.achievement_id=Achievements.achievements_id;`
-        db.connection.query(query, function(error, results, fields){
-            res.send(JSON.stringify(results));
-        })
-    });
+router.get('/', function (req, res) {
+    const query = `SELECT 
+        m.member_name, 
+        a.achievement_title
+   FROM Mem_Achieve_Details s
+   JOIN Members m on m.member_id = s.member_id
+   JOIN Achievements a on a.achievement_id = s.achievement_id`
+    db.connection.query(query, function (error, results, fields) {
+        res.send(JSON.stringify(results));
+    })
+});
 
 // ADD MEMBER_ACHIEVEMENT
-router.post('/add',function(req,res)
-{
+router.post('/add', function (req, res) {
     const data = req.body;
     query1 = `INSERT INTO Mem_Achieve_Details(member_id,achievement_id) VALUES ('${data.member_id}','${data.achievement_id}')`;
-    db.connection.query(query1, function(err,rows,fields){
-        if(err){
+    db.connection.query(query1, function (err, rows, fields) {
+        if (err) {
             console.log(err)
             res.sendStatus(400);
-        }
-        else
-        {
+        } else {
             query2 = `SELECT * FROM Mem_Achieve_Details;`;
-            db.connection.query(query2, function(err,rows,fields) {
-                if(err) {
+            db.connection.query(query2, function (err, rows, fields) {
+                if (err) {
                     console.log(err);
                     res.sendStatus(400);
-                }
-                else
-                {
+                } else {
                     res.send(JSON.stringify(rows));
                 }
             })
@@ -39,18 +38,18 @@ router.post('/add',function(req,res)
 });
 
 // DELETE MEMBER_ACHIEVEMENT
-router.delete('/delete/:id', function(req,res,next){
-  const member_id = req.params.id;
-  const deleteMember = `DELETE FROM Members WHERE member_id = ?`;
+router.delete('/delete/:id', function (req, res, next) {
+    const member_id = req.params.id;
+    const deleteMember = `DELETE FROM Members WHERE member_id = ?`;
 
-    db.connection.query(deleteMember, member_id, function(error, rows, fields){
+    db.connection.query(deleteMember, member_id, function (error, rows, fields) {
         if (error) {
-        res.sendStatus(400);
-        }
-        else {
+            res.sendStatus(400);
+        } else {
             res.sendStatus(200)
         }
-})});
+    })
+});
 
 
-  module.exports = router
+module.exports = router
