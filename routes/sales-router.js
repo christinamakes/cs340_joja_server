b0c50573371f6c709827e1bfb23f868fbf52853a
Devon Braner <68525598+orngepeel@users.noreply.github.com>
2022-08-07 21:48:54 -0400

    probably not

--- routes/sales-router.js
+++ routes/sales-router.js
@@ -41,9 +41,9 @@ router.put('/update-s', function(req,res,next){
     const member_id = parseInt(req.body.member_id);
     let employee_id = data.employee_id
     if (employee_id === '0') {
-        employee_id = 'NULL'
+        employee_id = `NULL`
     } else if (employee_id === null) {
-        employee_id = 'NULL'
+        employee_id = `NULL`
     } else {
         employee_id = parseInt(req.body.employee_id);
     }
@@ -54,9 +54,9 @@ router.put('/update-s', function(req,res,next){
     const queryUpdateSale = `UPDATE Sales SET ? WHERE order_number = ?`;
 
           // Run the 1st query
-    if (employee_id === 'NULL') {
+    if (employee_id === `NULL`) {
         
-        db.connection.query(queryUpdateSale, [{member_id:member_id, employee_id:'NULL', purchase_date:purchase_date,invoice_total:invoice_total},order_number], function(error, rows, fields){
+        db.connection.query(queryUpdateSale, [{member_id:member_id, employee_id:employee_id, purchase_date:purchase_date,invoice_total:invoice_total},order_number], function(error, rows, fields){
             if (error) {
                 // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
                 console.log(error);
