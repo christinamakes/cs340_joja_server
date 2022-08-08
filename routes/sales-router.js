5973371b66dfede3642e1e8432f08ba5c9422728
Devon Braner <68525598+orngepeel@users.noreply.github.com>
2022-08-07 21:29:15 -0400

    ?

--- routes/sales-router.js
+++ routes/sales-router.js
@@ -47,10 +47,10 @@ router.put('/update-s', function(req,res,next){
     const purchase_date = req.body.purchase_date;
     const invoice_total = parseInt(req.body.invoice_total);
 
-    const queryUpdateSale = `UPDATE Sales SET ? WHERE order_number = ? AND NULLIF('${data.employee_id}', '0')`;
+    const queryUpdateSale = `UPDATE Sales SET ? WHERE order_number = ?`;
 
           // Run the 1st query
-    db.connection.query(queryUpdateSale, [{member_id:member_id,employee_id:employee_id ,purchase_date:purchase_date,invoice_total:invoice_total},order_number], function(error, rows, fields){
+    db.connection.query(queryUpdateSale, [{member_id:member_id,employee_id:`NULLIF('${data.employee_id}', '0')` ,purchase_date:purchase_date,invoice_total:invoice_total},order_number], function(error, rows, fields){
         if (error) {
           // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
             console.log(error);
