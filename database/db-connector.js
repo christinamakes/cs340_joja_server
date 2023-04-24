88c5dbb274917978d93c5506ab53d31891ae55f3
Devon Braner <68525598+orngepeel@users.noreply.github.com>
2023-04-23 23:01:19 -0400

    cleanup

--- database/db-connector.js
+++ database/db-connector.js
@@ -4,8 +4,6 @@ require('dotenv').config()
 // Get an instance of mysql we can use in the app
 const mysql = require('mysql')
 
-console.log(process.env.DBDATABASE)
-
 const connection = mysql.createPool(process.env.MYSQL_URL);
 
 // Export it for use in our applicaiton
