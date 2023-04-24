a7886c0647188deb47f7b00faa55b713c579ac19
Devon Braner <68525598+orngepeel@users.noreply.github.com>
2023-04-23 20:25:16 -0400

    troubleshooting

--- database/db-connector.js
+++ database/db-connector.js
@@ -3,12 +3,7 @@
 // Get an instance of mysql we can use in the app
 const mysql = require('mysql')
 
-const connection = mysql.createConnection(/*{
-    host: process.env.DBHOST,
-    user: process.env.DBUSER,
-    password: process.env.DBPASSWORD,
-    database: process.env.DBDATABASE,
-  }*/"mysql://root:BH8KYMI7flbKT0EBnxEi@containers-us-west-24.railway.app:6605/railway");
+const connection = mysql.createPool('mysql://root:BH8KYMI7flbKT0EBnxEi@containers-us-west-24.railway.app:6605/railway');
 
 // Export it for use in our applicaiton
 module.exports.connection = connection;
